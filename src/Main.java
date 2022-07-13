759d621e7c5d68862ffb4d214670f0acbe2a42ab
Richard Castillo <rcvillegas1987@gmail.com>
2022-07-13 18:35:21 -0400

    ejercicios agregados en el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Sentencia IF
+        int numeroIf = 5;
+
+        if (numeroIf > 0) {
+            System.out.println("Numero Positivo");
+        }else if(numeroIf<0){
+            System.out.println("numero Negativo");
+        }else{
+            System.out.println("Numero igual a cero");
+        }
+        //CICLO WHILE
+        int numeroWhile=0;
+        while(numeroWhile<3)
+        {
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+        //CICLO DO-WHILE
+        do{
+            System.out.println("Ciclo do-while " + numeroIf);
+            numeroIf++;
+        }while(numeroIf<=5);
+
+        //CICLO FOR
+        int numeroFor=0;
+        for(numeroFor=0;numeroFor<=3;numeroFor++){
+            System.out.println("ciclo for : "+numeroFor);
+        }
+        //CICLO SWITCH
+        //
+        String estacion="ve3rano";
+        switch (estacion){
+            case "verano":
+                System.out.println("La estación del anio es : " +estacion);
+                break;
+            case "invierno":
+                System.out.println("La estación del anio es : " +estacion);
+                break;
+            case "otoño":
+                System.out.println("La estación del anio es : " +estacion);
+                break;
+            case "primavera":
+                System.out.println("La estación del anio es : " +estacion);
+                break;
+            default:
+                System.out.println("Ingreso una estacion incorrecta");
+
+        }
     }
 }
